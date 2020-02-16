c305a31c80f6a0081413e6a3a52708a88de5670e
CR <crkarthik11@gmail.com>
2020-02-17 00:57:47 +0530

    swagger-ui integration

--- DemoApp/src/main/java/com/thinkxfactor/demoApp/DemoAppApplication.java
+++ DemoApp/src/main/java/com/thinkxfactor/demoApp/DemoAppApplication.java
@@ -5,21 +5,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @ComponentScan("com.thinkxfactor")
 @EnableJpaRepositories(basePackages = "com.thinkxfactor")
 @EntityScan("com.thinkxfactor")
+@EnableSwagger2
 public class DemoAppApplication {
 
 	public static void main(String[] args)
 	{
 		ConfigurableApplicationContext context = SpringApplication.run(DemoAppApplication.class, args);
 
+	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 }
